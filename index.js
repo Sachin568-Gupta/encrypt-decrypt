64238cda3efa0f1ee1077dab2021bb7fce0f2cfb
Sachin Gupta <sachin.gupta@resourcifi.com>
2022-03-30 15:40:19 +0530

    updates encryption decryption

---
+++ index.js
@@ -0,0 +1,47 @@
+class Encryption {
+  cipher = (salt) => {
+    const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+    const byteHex = (n) => ("0" + Number(n).toString(16)).substr(-2);
+    const applySaltToChar = (code) =>
+      textToChars(salt).reduce((a, b) => a ^ b, code);
+    return (text) =>
+      text
+        .split("")
+        .map(textToChars)
+        .map(applySaltToChar)
+        .map(byteHex)
+        .join("");
+  };
+  decipher = (salt) => {
+    const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+    const applySaltToChar = (code) =>
+      textToChars(salt).reduce((a, b) => a ^ b, code);
+    return (encoded) =>
+      encoded
+        .match(/.{1,2}/g)
+        .map((hex) => parseInt(hex, 16))
+        .map(applySaltToChar)
+        .map((charCode) => String.fromCharCode(charCode))
+        .join("");
+  };
+
+  encryption_key(secret_key, text) {
+    const myCipher = cipher(secret_key);
+    return myCipher(text);
+  }
+  decryption_key(secret_key, text) {
+    const myDecipher = decipher(secret_key);
+    return myDecipher(text);
+  }
+}
+
+module.exports = Encryption;
+
+// To create a cipher
+// const myCipher = cipher('mySecretSalt')
+//Then cipher any text:
+// let data = myCipher('sachin@gmail.com');
+// console.log(data)
+//To decipher, you need to create a decipher and use it:
+// const myDecipher = decipher('mySecretSalt')
+// console.log(myDecipher(data))
